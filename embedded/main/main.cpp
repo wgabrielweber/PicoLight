774a734157180b6e5accc3d97604440b2c9982f5
Gabriel Weber <wgabrielweber@gmail.com>
2023-11-21 13:09:35 -0300

    add Logger to main

--- embedded/main/main.cpp
+++ embedded/main/main.cpp
@@ -1,9 +1,23 @@
+/**
+ * @file main.hpp
+ * @brief Main file of the embedded system
+ * @author Gabriel Weber Berwian <wgabrielweber@hotmail.com>
+ * @date 19/11/2023
+ * @institution UFSC
+ */
+
+/* 
+ *  Embedded System Main File
+ */
+
 #include <stdio.h>
 #include "pico/stdlib.h"
 
 #include <PicoLed.hpp>
-#include "ClockCalendar.hpp"
+#include "PicoLed.hpp"
 #include "BH1750FVI.hpp"
+#include "Control.hpp"
+#include "Logger.hpp"
 #include "hardware/rtc.h"
 #include "pico/stdlib.h"
 #include "pico/util/datetime.h"
@@ -16,15 +30,12 @@ int main()
     stdio_init_all();
     sleep_ms(1000);
 
-    char datetime_buf[256];
-    char *datetime_str = &datetime_buf[0];
-
-    // Start on Sunday 19th of November 2023 15:45:00
+    // Structure for working with the given rtc
     datetime_t t = {
             .year  = 2023,
             .month = 11,
             .day   = 19,
-            .dotw  = 0, // 0 is Sunday, so 5 is Friday
+            .dotw  = 0,
             .hour  = 15,
             .min   = 45,
             .sec   = 00
@@ -38,50 +49,75 @@ int main()
     // tbe delay is up to 3 RTC clock cycles (which is 64us with the default clock settings)
     sleep_us(64);
 
-    BH1750FVI lightSensor(BH1750FVI::devAddr_L, BH1750FVI::contHighRes); 
-
+    // Initialize your LED strip, sensor, and other components
+    // Replace the placeholder values with your actual initialization parameters
+    auto ledStrip = PicoLed::addLeds<PicoLed::WS2812B>(pio0, 0, LED_PIN, LED_LENGTH, PicoLed::FORMAT_GRB);
+    BH1750FVI lightSensor(BH1750FVI::devAddr_L, BH1750FVI::contHighRes);
     lightSensor.setSDAPin(16);
     lightSensor.setSCLPin(17);
-
     lightSensor.begin();
 
-    // 0. Initialize LED strip
-    printf("0. Initialize LED strip\n");
-    auto ledStrip = PicoLed::addLeds<PicoLed::WS2812B>(pio0, 0, LED_PIN, LED_LENGTH, PicoLed::FORMAT_GRB);
-    ledStrip.setBrightness(50);
-    printf("1. Clear the strip!\n");
-    
     // 1. Set all LEDs to ambient 1!
     printf("1. Set all LEDs to ambient 1!\n");
     ledStrip.fill( PicoLed::RGB(255, 220, 185) );
+    ledStrip.setBrightness(0);      // This initiates the led strip off
     ledStrip.show();
     sleep_ms(500);
 
-    while(1) {
+    // Create an instance of the Control class
+    PicoLed::PicoLedController ledStripController(ledStrip);
+    Control controller(ledStripController, lightSensor);
 
-        for (uint16_t bright_lvl = 0; bright_lvl <= 250; bright_lvl += 25) {
-            ledStrip.setBrightness(bright_lvl);
-            ledStrip.show();
-            //printf("Brightness Level:\n", ledStrip.getBrightness());
-            sleep_ms(250);  // Adjust the sleep duration as needed
-        }
-        
+    // Set PID gains
+    controller.setPIDGains(4, 0.05, 2);
+
+    // Set smoothing factor (adjust as needed)get
+    controller.setSmoothingFactor(1.0);
+
+    // Set the target light intensity
+    controller.setTargetLightIntensity(15000);  // Set the target light intensity
+
+    controller.setMaxBrightnessChange(1);  // Set the desired maximum change (Brightness levels goes from 0 to 255)
+
+    // Timestamp for the buffer
+    char timestampBuffer[20];
+
+    // Object logger for the Logger class
+    Logger logger;
+
+    std::vector<std::string> logs;  // Container to store logs
+
+    // Main loop
+    while (1) {    
+
+        // Retrieve and print the light intensity
+        controller.adjustBrightness();
+        printf("\n\rLight Intensity: %u lux    Brightness: %u     ", controller.getLightIntensity(), ledStrip.getBrightness());
+        //printf("Brightness: %u \n", ledStrip.getBrightness());
+
+        // get a new date and time for the timestamp
         rtc_get_datetime(&t);
-        datetime_to_str(datetime_str, sizeof(datetime_buf), &t);
-        printf("\r%s      \n", datetime_str);
 
-        uint16_t lightIntensity = lightSensor.getLux();
-        printf("Light Intensity: %u lux\n", lightIntensity);
+        // Converting the base datetime_t members into the ISO8601 standard
+        snprintf(timestampBuffer, sizeof(timestampBuffer), "%d-%02d-%02dT%02d:%02d:%02d",
+             t.year, t.month, t.day, t.hour, t.min, t.sec);
+        printf("%s\n", timestampBuffer);
 
-        for (uint16_t bright_lvl = 250; bright_lvl > 0; bright_lvl -= 25) {
-            ledStrip.setBrightness(bright_lvl);
-            ledStrip.show();
-            //printf("Brightness Level:\n", ledStrip.getBrightness());
-            sleep_ms(250);  // Adjust the sleep duration as needed
+        // Time based condition to create a new log
+        bool newLog;
+        newLog = t.sec == 0 || t.sec == 15 || t.sec == 30 || t.sec == 45;
+        if (newLog == true) {
+            // Adding logs to the container
+            logs.push_back(std::string(timestampBuffer) + " : Light Intensity: " + std::to_string(controller.getLightIntensity()) + " lux : Brightness: " + std::to_string(ledStrip.getBrightness()));
+
         }
 
-        lightIntensity = lightSensor.getLux();
-        printf("Light Intensity: %u lux\n", lightIntensity);
+        // Print the size of the logs vector
+        printf("Logs vector size: %zu\n", logs.size());
+
+        // Sleep between iterations
+        sleep_ms(10);
     }
+
     return 0;
 }
